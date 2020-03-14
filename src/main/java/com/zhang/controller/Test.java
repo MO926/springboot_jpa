94393b61c1590fe140b949270fac62f68f6bc3d3
zhang <1364397024@qq.com>
2020-03-14 09:41:20 +0800

    测试代码

---
+++ src/main/java/com/zhang/controller/Test.java
@@ -0,0 +1,13 @@
+package com.zhang.controller;
+
+/**
+ * @author zhangjingfeng
+ * @version 1.0
+ * @date 2020/3/14 9:38
+ * @Description:
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("测试代码");
+    }
+}
